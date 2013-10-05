d47820905123f8324a447e38cfaeb3deaa8566ea
Nathan Woods <big.nate.w@gmail.com>
2013-10-05 10:38:58 -0600

    dialog login changes (considering removing force login)

--- htdocs/js/all.js
+++ htdocs/js/all.js
@@ -1,20 +1,17 @@
 var General = {
 	login: function() {
-		var dialog = $(document.createElement('div'));
-		dialog.html('<form id="webLogin"><table><tbody><tr><td><label for="username">Username:</label></td><td width="100%"><input id="username" type="text" name="user" style="width:100%"></td></tr><tr><td><label for="password">Password:</label></td><td><input id="password" type="password" name="pass" style="width:100%"></td></tr><tr><td colspan="2" id="response"><input type="submit" style="position: absolute; left: -99px"/><!--hide from most browsers--></td></tr></tbody></table></form>')
+		var dialog = $('<div>');
+		dialog.html('<form id="webLogin"><table><tbody><tr><td><label for="username">Username:&nbsp;</label></td><td width="100%"><input id="username" type="text" name="user" style="width:100%"></td></tr><tr><td><label for="password">Password:&nbsp;</label></td><td><input id="password" type="password" name="pass" style="width:100%"></td></tr><tr><td colspan="2" id="response"><input type="submit" style="position: absolute; left: -99px"/><!--hide from most browsers--></td></tr></tbody></table></form>')
 			.attr('title','Web Administration Login')
 			.dialog({ resizable: false, draggable: false, modal: true, closeOnEscape: false,
-				buttons: {
-					'Login': function() {
-						$('#webLogin', dialog).submit();
-					},
-					Cancel: function() {
-						dialog.dialog('close');
-					}
-				},
+				buttons: General.buttonObject,
 				open: function(event, ui) { 
 					$('.ui-dialog-titlebar-close').hide();
 					$('#webLogin', dialog).validate({
+						rules: {
+							user: { required: true, minlength: 2 },
+							pass: { required: true, minlength: 2 }
+						},
 						submitHandler: function(form) {
 							dialog.dialog('option', 'buttons', { } );
 							
@@ -24,16 +21,15 @@ var General = {
 								data: 'action=login&user=' + form.user.value + '&pass=' + form.pass.value + '&referer=blank',
 								dataType:'json',
 								success:function(json){
-									//alert(json.reload + "-" + (json.reload === true) + "-" + (json.reload === false));
-									$('#response', '#webLogin').html(json.msg);
+									$( '#response', '#webLogin' ).html( json.msg );
 									
-									if (json.reload) {
-										setTimeout("window.location.reload();", 4000);
+									if ( json.reload ) {
+										setTimeout("window.location.reload();", 1000);
 									} else {
 										setTimeout("$('#response', '#webLogin').html('')", 4000);
-										// reset buttons?
+										$('#password').val('');
+										dialog.dialog('option', 'buttons', General.buttonObject );
 									}
-									//dialog.dialog('close'); - reload will take care of this
 								}
 							});
 						}
@@ -44,7 +40,7 @@ var General = {
 		return false;
 	},
 	logout: function() {
-		var dialog = $(document.createElement('div'));
+		var dialog = $('<div>');
 		dialog.html('<form id="webLogout"><div id="response"></div></form>')
 			.attr('title','Web Administration Logout')
 			.dialog({ resizable: false, draggable: false, modal: true, closeOnEscape: false,
@@ -58,7 +54,7 @@ var General = {
 							dataType:'json',
 							success:function(json){
 								$('#response', '#webLogout').html(json.msg);
-								setTimeout("window.location.reload();", 4000);
+								setTimeout("window.location.reload();", 1000); // remove query string here
 								//dialog.dialog('close');
 							}
 						});//*/
@@ -71,6 +67,14 @@ var General = {
 				close: function() { dialog.dialog( 'destroy' ); dialog.remove(); }
 			});
 		return false;
+	},
+	buttonObject: {
+		'Login': function() {
+			$('#webLogin', this).submit();
+		},
+		Cancel: function() {
+			$(this).dialog('close');
+		}
 	}
 };
 
