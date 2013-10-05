02704d2a98e0e45f6831969a4a6d48c665b89cf3
Nathan Woods <big.nate.w@gmail.com>
2013-10-05 15:20:17 -0600

    Strict standards + cleanup

--- htdocs/edit.php
+++ htdocs/edit.php
@@ -2,24 +2,15 @@
 include("../libinc/edit.php");
 
 if (User::verify() == '') die("Your Kung-Fu is not strong!"); // ensure authenticated
-// now ensure rights to edit
-if (!isset($_REQUEST['action'])) die("Your kung-fu is no good here");
+
+if (!isset($_REQUEST['action'])) die("Your kung-fu is no good here"); // now ensure rights to edit
 
 $edt = new Edit();
 switch ($_REQUEST['action']) {
-	case 'updateOrder':
-		echo $edt->updateOrder($_REQUEST['data']);
-		break;
-	case 'removeContent':
-		echo $edt->removeContent($_REQUEST['remID']);
-		break;
-	case 'addContent':
-		echo $edt->addContent($_REQUEST['vfsID'], $_REQUEST['loc'], $_REQUEST['blockID']);
-		break;
-	case 'updateContent':
-		echo $edt->updateContent($_REQUEST['cID'], $_REQUEST['content']);
-		break;
+	case 'updateOrder': echo $edt->updateOrder($_REQUEST['data']); break;
+	case 'removeContent': echo $edt->removeContent($_REQUEST['remID']); break;
+	case 'addContent': echo $edt->addContent($_REQUEST['vfsID'], $_REQUEST['loc'], $_REQUEST['blockID']); break;
+	case 'updateContent': echo $edt->updateContent($_REQUEST['cID'], $_REQUEST['content']); break;
 	
-	default:
-		echo "Your Kung-Fu is not strong!";
+	default: echo "Your Kung-Fu is not strong!";
 }
