8b66b8c7e720b280bff4b2337b25df828ce4567e
Nathan Woods <big.nate.w@gmail.com>
2013-10-05 07:47:16 -0600

    strict standards adherance

--- htdocs/edit.php
+++ htdocs/edit.php
@@ -4,18 +4,20 @@ include("../libinc/edit.php");
 if (User::verify() == '') die("Your Kung-Fu is not strong!"); // ensure authenticated
 // now ensure rights to edit
 if (!isset($_REQUEST['action'])) die("Your kung-fu is no good here");
+
+$edt = new Edit();
 switch ($_REQUEST['action']) {
 	case 'updateOrder':
-		echo Edit::updateOrder($_REQUEST['data']);
+		echo $edt->updateOrder($_REQUEST['data']);
 		break;
 	case 'removeContent':
-		echo Edit::removeContent($_REQUEST['remID']);
+		echo $edt->removeContent($_REQUEST['remID']);
 		break;
 	case 'addContent':
-		echo Edit::addContent($_REQUEST['vfsID'], $_REQUEST['loc'], $_REQUEST['blockID']);
+		echo $edt->addContent($_REQUEST['vfsID'], $_REQUEST['loc'], $_REQUEST['blockID']);
 		break;
 	case 'updateContent':
-		echo Edit::updateContent($_REQUEST['cID'], $_REQUEST['content']);
+		echo $edt->updateContent($_REQUEST['cID'], $_REQUEST['content']);
 		break;
 	
 	default:
